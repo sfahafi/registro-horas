c4ef8bea38d66414879214a7e42fb169706f6536
Sharbel <sfahafi18@gmail.com>
2022-03-28 13:45:30 -0300

    Primer commit

---
+++ src/test/java/com/carga/horas/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carga.horas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
